06803da64876f5450b2136e05472e11e712ebd73
arskii <konstofff@gmail.com>
2023-04-04 20:59:51 +0200

    Added localization i18n to app

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
+import { Locale } from "../../i18n-config";
+import { getDictionary } from "../../get-dictionary";
 import MainButton from "./components/MainButton";
 import SecondButton from "./components/SecondButton";
 import Articles from "./articles/page";
@@ -7,20 +9,28 @@ import About from "./about/page";
 import Price from "./price/page";
 import Galery from "./galery/page";
 
-export default function Home() {
+export default async function Home({
+  params: { lang },
+}: {
+  params: { lang: Locale };
+}) {
+  const dictionary = await getDictionary(lang);
   return (
     <div className="container mx-auto">
       <section className="flex justify-between items-center py-10">
         <div className="flex flex-col xl:w-96 lg:w-72 gap-y-5">
           <h1 className="font-serif font-bold uppercase xl:text-5xl lg:text-4xl">
-            HUNDEERZIEHUNG
+            {dictionary["home"].h1}
           </h1>
-          <p className=" text-left text-lg">
-            Wir schulen Besitzer darin, Hunde so zu erziehen, dass das
-            Zusammenleben mit einem Haustier angenehm ist.
-          </p>
-          <MainButton text="Online termin" link="/meeting" />
-          <SecondButton text="Beratungsfragebogen" link="/form" />
+          <p className=" text-left text-lg">{dictionary["home"].p}</p>
+          <MainButton
+            text={dictionary["home"]["main-button"]}
+            link={`/${lang}/meeting`}
+          />
+          <SecondButton
+            text={dictionary["home"]["secont-button"]}
+            link={`/${lang}/form`}
+          />
           <Image
             className="mt-18"
             src="/foots.svg"
